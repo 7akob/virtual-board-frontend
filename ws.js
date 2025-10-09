7565e2b7bff9f9bad6bcdc402638ee4fef6ed480
Jakob Sandberg <sandbeja@arcada.fi>
2025-10-09 15:28:33 +0300

    Uppdaterat hantering av olika inloggningar

--- ws.js
+++ ws.js
@@ -1,30 +1,57 @@
 const token = localStorage.getItem('authToken');
 const ws = new WebSocket(`wss://pastebin-websocket.onrender.com?token=${token}`);
 
+const OutEl = document.querySelector('#out');
+const errEl = document.querySelector('#err');
+const inputEl = document.querySelector('#messageInput');
+const sendBtn = document.querySelector('#sendButton');
+
 ws.onopen = function (event) {
     console.log('Connected to WebSocket server!');
 };
 
 ws.onmessage = function (event) {
     console.log('Recived message: ', event.data);
-    const data = JSON.parse(event.data);
-
-    if (data.status == 0) {
-        document.querySelector('#out').innerHTML = data.msg;
-        document.querySelector('#err').innerHTML = '';
-    } else {
-        document.querySelector('#err').innerHTML = data.msg;
+    
+    let data;
+    try {
+        data = JSON.parse(event.data);
+    } catch (err) {
+        console.error('Invalid JSON: ', err);
+        return;
     }
-}
+
+    switch (data.type) {
+        case 'init':
+            OutEl.textContent = data.text || '';
+            errEl.textContent = '';
+            break;
+        
+        case 'update':
+            OutEl.textContent = data.text || '';
+            errEl.textContent = '';
+            break;     
+            
+        case 'error':
+            errEl.textContent = data.message;
+            break;
+
+        default:
+            console.warn('Unknown message type: ', data.type);
+    }
+};
 
 ws.onclose = function (event) {
     console.log('Connection closed!');
 };
 
-document.querySelector('#sendButton').addEventListener('click', () => {
-    const message = document.querySelector('#messageInput').value;
-    if (message) {
-        ws.send(message);
-        document.querySelector('#messageInput').value = '';
+sendBtn.addEventListener('click', () => {
+    const message = inputEl.value.trim();
+    if(message && ws.readyState == WebSocket.OPEN) {
+        ws.send(JSON.stringify({
+            type: 'update',
+            text: message
+        }));
+        inputEl.value = '';
     }
 })
